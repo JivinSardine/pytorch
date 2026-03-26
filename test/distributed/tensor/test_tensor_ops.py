377dddd187f9b385ef9a1778b63f556f82159eb4
Anshul Sinha <anshulsi@meta.com>
2026-03-25 19:38:37 -0700

    [dtensor][index_ops] adding index_fill and index_reduce strategies
    ghstack-source-id: 5dd86a4ffa8b1b2691556f8d476c7c1ef294e18a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/178456

--- test/distributed/tensor/test_tensor_ops.py
+++ test/distributed/tensor/test_tensor_ops.py
@@ -20,8 +20,8 @@ from torch.distributed.tensor._sharding_prop import ShardingPropagator
 from torch.distributed.tensor.debug import CommDebugMode
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_utils import (
-    MI200_ARCH,
     instantiate_parametrized_tests,
+    MI200_ARCH,
     parametrize,
     run_tests,
     serialTest,
@@ -973,6 +973,42 @@ class DistTensorOpsTest(DTensorContinuousTestBase):
         else:
             ref = op(global_input, 1, idx, global_source)
             output_dt = op(input_dt, 1, idx_dt, source_dt)
+
+        self.assertIsInstance(output_dt, DTensor)
+        self.assertEqual(output_dt.full_tensor(), ref)
+
+    def test_index_fill(self):
+        """Test index_fill with sharded input."""
+        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
+        global_input = torch.randn(4, 8, device=self.device_type)
+        idx = torch.tensor([1, 3], device=self.device_type)
+        fill_value = 5.0
+
+        ref = global_input.index_fill(1, idx, fill_value)
+
+        input_dt = distribute_tensor(global_input, device_mesh, [Shard(0)])
+        idx_dt = distribute_tensor(idx, device_mesh, [Replicate()])
+
+        output_dt = input_dt.index_fill(1, idx_dt, fill_value)
+
+        self.assertIsInstance(output_dt, DTensor)
+        self.assertEqual(output_dt.full_tensor(), ref)
+
+    @parametrize("reduce_op", ["mean", "amax", "amin", "prod"])
+    def test_index_reduce(self, reduce_op):
+        """Test index_reduce with sharded input."""
+        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
+        global_input = torch.randn(4, 8, device=self.device_type)
+        idx = torch.tensor([1, 3], device=self.device_type)
+        global_source = torch.randn(4, 2, device=self.device_type)
+
+        ref = global_input.index_reduce(1, idx, global_source, reduce_op)
+
+        input_dt = distribute_tensor(global_input, device_mesh, [Shard(0)])
+        idx_dt = distribute_tensor(idx, device_mesh, [Replicate()])
+        source_dt = distribute_tensor(global_source, device_mesh, [Shard(0)])
+
+        output_dt = input_dt.index_reduce(1, idx_dt, source_dt, reduce_op)
 
         self.assertIsInstance(output_dt, DTensor)
         self.assertEqual(output_dt.full_tensor(), ref)
